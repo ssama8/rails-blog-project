096caf3358a2c2714dcb21e989dca1a0a79bec4f
ssama8 <saketsama8@gmail.com>
2022-04-10 23:04:42 -0700

    made form with validation to create articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,37 @@
 class ArticlesController < ApplicationController
   def show
-   byebug
     @article = Article.find(params[:id])
    
   end
   def index
+    @all_articles = Article.all
   end
+  def new
+    @article = Article.new
+  end
+  def create
+    puts "Processing"
+    
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
+        format.json { render :show, status: :created, location: @article }
+
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+    # if @article.save
+    #   redirect_to article_path(@article)
+    # else
+    #    format.html { render :new, status: :unprocessable_entity }
+    #     format.json { render json: @article.errors, status: :unprocessable_entity }
+    # end
+
+  end
+
+
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 6, maximum: 75}
-  validates :description, presence: true, length: {minimum: 20, maximum: 500}
+  validates :description, presence: true, length: {minimum: 10, maximum: 500}
 
 end
