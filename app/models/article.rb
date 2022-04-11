d386d7c00914f5d3ba11ca3259f86e5f1af59bc0
ssama8 <saketsama8@gmail.com>
2022-04-10 20:12:52 -0700

    Created route to articles/:id

---
+++ app/models/article.rb
@@ -0,0 +1,5 @@
+class Article < ApplicationRecord
+  validates :title, presence: true, length: {minimum: 6, maximum: 75}
+  validates :description, presence: true, length: {minimum: 20, maximum: 500}
+
+end
