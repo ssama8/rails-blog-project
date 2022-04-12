099e6d1a77ac714fe4c9b6f6c2e4c0a83bac0843
ssama8 <saketsama8@gmail.com>
2022-04-11 17:33:31 -0700

    updated routing and added private methods to reuse code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update, :destoy]
   def show
-    @article = Article.find(params[:id])
-   
   end
   def index
     @all_articles = Article.all
@@ -9,29 +8,49 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  def edit
+  end
   def create
-    puts "Processing"
-    
-    @article = Article.new(params.require(:article).permit(:title, :description))
-    
+    @article = Article.new(article_params)
     respond_to do |format|
       if @article.save
         format.html { redirect_to article_url(@article), notice: "Article was successfully created." }
         format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+ 
+  end
+
+  def update
+  respond_to do |format|
+      if  @article.update(article_params)
+        format.html { redirect_to article_url(@article), notice: "Article was successfully updated." }
+        format.json { render :show, status: :updated, location: @article }
 
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
-    # if @article.save
-    #   redirect_to article_path(@article)
-    # else
-    #    format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @article.errors, status: :unprocessable_entity }
-    # end
-
+ 
+    
   end
 
+  def destroy
+    @article.destroy 
+    redirect_to articles_path
+  end
+
+  private 
+  def find_article
+    @article = Article.find(params[:id])
+  end  
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get 'about', to: 'pages#about' 
   # get 'articles' to: 'articles#view'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
